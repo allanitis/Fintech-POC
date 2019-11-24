39ef16cf937c16a7629ea4466fea92345a3e2e8a
allanitis <matthewa021@gmail.com>
2019-11-24 14:08:18 +0000

    Add requsets dependency

--- Fintech-POC/Examples/CurrencyCloud_Authentication.py
+++ Fintech-POC/Examples/CurrencyCloud_Authentication.py
@@ -1,3 +1,16 @@
+import requests
+
 def handler(event, context):
+    try:
+        res = requests.post(
+            "https://devapi.currencycloud.com/v2/authenticate/api",
+            params={},
+            headers={"Accept":"application/json","Content-Type":"application/x-www-form-urlencoded"},
+            data="login_id=matt%40fintechsandpit.com&api_key=e48a2102f918bab738e327af297f5f335231ffb0803d4d513a6b7a808f0d2c19"
+        )
+        print(res.body)
+    except BaseException as e:
+        # error handling goes here
+        raise(e)
     
     return {"message": "Successfully executed"}
