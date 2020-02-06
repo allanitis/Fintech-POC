8d9930df9d957d3003f51485e307a49605e5ba01
allanitis <matthewa021@gmail.com>
2020-02-06 12:12:33 +0000

    CurrencyCloud conversion

--- Fintech-POC/Demo/CC_Auth.py
+++ Fintech-POC/Demo/CC_Auth.py
@@ -1,7 +1,7 @@
 from botocore.vendored import requests
 import os
 
-def handler(event, context):
+def getAuthToken():
     try:
         res = requests.post(
             "https://devapi.currencycloud.com/v2/authenticate/api",
@@ -9,9 +9,8 @@ def handler(event, context):
             headers={"Accept":"application/json","Content-Type":"application/x-www-form-urlencoded"},
             data="login_id={}&api_key={}".format(os.environ["CC_LOGIN"], os.environ["CC_KEY"])
         )
-        print(res.json())
     except BaseException as e:
         # error handling goes here
         raise(e)
     
-    return {"message": "Successfully executed"}
+    return res.json()
---
+++ Fintech-POC/Demo/CurrencyCloud_convert.py
@@ -0,0 +1,22 @@
+from botocore.vendored import requests
+import CC_Auth
+
+def handler(event, context):
+    cc_key = CC_Auth.getAuthToken()['auth_token']
+    try:
+        res = requests.post(
+            "https://devapi.currencycloud.com/v2/conversions/create",
+            params={},
+            headers={"X-Auth-Token":cc_key,"Accept":"application/json","Content-Type":"application/x-www-form-urlencoded"},
+            data="buy_currency=USD&sell_currency=EUR&fixed_side=buy&amount=12000&term_agreement=true"
+        )
+        return res.json()
+    except BaseException as e:
+        # error handling goes here
+        raise(e)
+
+    
+
+
+    
+    return {"message": "Successfully executed"}
